e54a7f6e557e2b9fc55739c96cb53021789fccf2
mohimohi1 <mo.misha@bk.ru>
2019-05-20 06:54:59 +0300

    1

---
+++ VKClienMohiMo/Controllers/GroupsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GroupsViewController.swift
+//  VKClienMohiMo
+//
+//  Created by Михаил Мохнач on 19/05/2019.
+//  Copyright © 2019 Михаил Мохнач. All rights reserved.
+//
+
+import UIKit
+
+class GroupsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+  
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    @IBAction func myUnwindAction1 (UnwindSegue: UIStoryboardSegue) {
+        
+    }
+
+}
